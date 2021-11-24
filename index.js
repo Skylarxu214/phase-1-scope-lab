af131c926e2968d04d9bc9363b82b632d44609a7
Tian Xu <xueqingxu0302@gmail.com>
2021-11-24 10:08:25 -0700

    commit tree 79b2e4b73c9bcadad8db8a9a1656002e66628d4c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  return customerName = "BOB"
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+   return "hi"
+
+}
+
+function overwriteBestCustomer(){
+    
+    return bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Amy"
+}
